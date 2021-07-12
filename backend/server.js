ec228e55c80d91ecb62a9d3b8685e4bc97243e5c
Yves Loeys <yves.loeys@gmail.com>
2021-07-12 12:21:15 -0700

    server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose')
 const morgan = require('morgan');
-//const session = require('express-session')
+
 
 require('dotenv').config();
 
@@ -11,24 +11,12 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json());
-// app.set("trust proxy", 1);
-// app.use(
-//   session({
-//     secret: process.env.SESSION_KEY || 'Super Secret (change it)',
-//     resave: true,
-//     saveUninitialized: false,
-//     cookie: {
-//       sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
-//       secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
-//     }
-//   })
-// );
+
 
 //Gives you a log of the endpoints you hit
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
 
 
 const uri = process.env.MONGO_URI;
@@ -37,6 +25,7 @@ const connection = mongoose.connection;
 connection.once('open', ()=> {
   console.log("MongoDB database connection success");
 })
+
 
 const backgroundRouter = require('./routes/background.js');
 app.use('/background',backgroundRouter)
