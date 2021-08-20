f9a713cc1db12d98c6d928069e1c0054be0416ec
Yves Loeys <yves.loeys@gmail.com>
2021-08-19 22:28:38 -0700

    bugfix-heroku

--- backend/server.js
+++ backend/server.js
@@ -38,16 +38,16 @@ const backgroundRouter = require('./routes/background.js');
 app.use('/background',backgroundRouter)
 
 const portfolioRouter = require('./routes/portfolio.js');
-app.use('/port',portfolioRouter)
+app.use('/portfolio',portfolioRouter)
 
 const trendingRouter = require('./routes/trending.js');
-app.use('/trend',trendingRouter)  
+app.use('/trending',trendingRouter)  
 
 const infoRouter = require('./routes/info.js');
 app.use('/info',infoRouter)
 
 const contactRouter = require('./routes/contact.js');
-app.use('/cont',contactRouter)
+app.use('/contact',contactRouter)
 
 
 
