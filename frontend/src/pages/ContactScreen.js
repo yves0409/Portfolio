71e35f610e8d66fd803a2b24a8c0f284516b3980
Yves Loeys <yves.loeys@gmail.com>
2021-09-14 13:08:25 +0200

    validation emailform

--- frontend/src/pages/ContactScreen.js
+++ frontend/src/pages/ContactScreen.js
@@ -9,6 +9,7 @@ import ContactItem from '../components/ContactItem'
 import info from "../data/info"
 import emailjs from 'emailjs-com';
 import SendIcon from '@material-ui/icons/Send';
+import { useForm } from "react-hook-form";
 
 init("user_dvXegxEb11cRFMAuFpf6J");
 
@@ -17,7 +18,8 @@ const ContactScreen = () => {
     const [subject, setSubject] = useState('');
     const [emailsender, setEmailsender] = useState('');
     const [question, setQuestion] = useState('');
-
+    const { register, handleSubmit,formState:{errors} } = useForm({mode:"onBlur"});
+    const onFormSubmit  = (data) => console.log(data);
   
  const templateParams = {
           from_name: emailsender,
@@ -29,7 +31,8 @@ const ContactScreen = () => {
 
 
 //SEND FORM TO EMAIL => 'emailjs'////  
-const sendMessage = (e) => {
+
+ const sendMessage = (e) => {
     e.preventDefault();
         setName("");
         setEmailsender("");
@@ -39,7 +42,7 @@ const sendMessage = (e) => {
     emailjs.send(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID,templateParams,process.env.REACT_APP_USER_ID)
      .then(
               function(response) {
-                  toast.success("📩 Your message has successfully sent!", {
+                  toast.success(`📩 Your message has successfully sent!`, {
                   position: toast.POSITION.TOP_RIGHT,
                   pauseOnHover: true
                })
@@ -51,6 +54,11 @@ const sendMessage = (e) => {
                   console.log(err);
               })
         };
+    
+        
+       
+       
+      
 
 
 return (
@@ -59,61 +67,47 @@ return (
             <Title title={"Contact"} span={"Contact"}/>
             <ContactPageStyled>
             <InnerLayout className="contact-section">
-                <div className="left-content">
+              <div className="left-content">
                     <div className="contact-title">
                         <h4>Get in touch  </h4>
-                        
                     </div>
-                        <form className="form" id="contactform"   >
-                                <div className="form-field">
-                                    <label htmlFor="name">Enter name</label>
-                                    <input type="text" id="name" name="name" value={name} required minLength="6" maxLength="8" onChange={(e)=>setName(e.target.value)}/>
-                                </div>
-                                    <div className="form-field">
-                                        <label htmlFor="name">Enter email</label>
-                                        <input type="email" id="email" name="email" value={emailsender} onChange={(e)=> setEmailsender(e.target.value)}/>
-                                    </div>
-                                        <div className="form-field">
-                                            <label htmlFor="subject">Enter subject</label>
-                                            <input type="text" id="subject" name="subject" value={subject} onChange={(e)=> setSubject(e.target.value)}/>
-                                        </div>
-                                    <div className="form-field">
-                                    <label htmlFor="subject">Enter message</label>
-                                           <textarea  id="textarea" cols="30" rows="10" name="question" value={question} onChange={(e)=> setQuestion(e.target.value)}></textarea>
-                                    </div>
-                                <div className="form-field" >
-                                    <div className="iconbtn" data-toggle="tooltip" data-placement="bottom" title="send email">
-                                       <SendIcon onClick={sendMessage} />
-                                    </div>
-                                 </div>
-                         </form>
-                         
+                        
+                    <form className="form" id="contactform" onSubmit={sendMessage} >
+                        <div className="form-field">
+                            <label htmlFor="name">Enter name <span className="requiredStar">*</span></label>
+                            <input className={errors.name ? 'err' : ""}type="text" id="name" name="name" value={name} {...register("name",{required:'name is required'})}  onChange={(e)=>setName(e.target.value)}/>
+                            {errors.name && <span className="errorMessage">Name is required</span>}
                         </div>
-                        {/* change */}
-                        {/* <div class="container">
-	<div className="front side">
-		<div className="content">
-			<h1>Yves Loeys</h1>
-			<p>specialised in front end web design, user experience and creating identities. Throughout my career, I have worked with companies of all shapes and sizes that enriched my experience
-			</p>
-		</div>
-	</div>
-	<div className="back side">
-		<div className="content">
-			<h1>Contact Me</h1>
-			<form>
-				<label>Your Name :</label>
-				<input type="text" placeholder="Yves Loeys"/>
-				<label>Your E-mail :</label>
-				<input type="text" placeholder="yves.loeys@mail.com"/>
-				<label>Your message :</label>
-				<textarea placeholder="Subject"></textarea>
-				<input type="submit" value="Done"/>
-			</form>
-		</div>
-	</div> */}
-{/* change */}
-{/* </div> */}
+                        <div className="form-field">
+                            <label htmlFor="name">Enter email <span className="requiredStar">*</span></label>
+                            <input className={errors.email ? 'err' : ""} type="email" id="email" name="email" value={emailsender} {...register("email",{
+                                required: 'Email is required',
+                                pattern: {
+                                value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                message: 'Please enter a valid eeemail',
+                                },
+                            })} onChange={(e)=> setEmailsender(e.target.value)}/>
+                            {errors.email && <span className="errorMessage">Please enter a valid email</span>}
+                        </div>
+                        <div className="form-field">
+                            <label htmlFor="subject">Enter subject <span className="requiredStar">*</span></label>
+                            <input className={errors.subject ? 'err' : ""} type="text" id="subject" name="subject" value={subject} {...register("subject",{required:'subject is required'})} onChange={(e)=> setSubject(e.target.value)}/>
+                            {errors.subject && <span className="errorMessage">Please enter a subject</span>}
+                        </div>
+                        <div className="form-field">
+                        <label htmlFor="subject">Enter message (Max 150 char) <span className="requiredStar">*</span></label>
+                                <textarea className={errors.question ? 'err' : ""} id="textarea" cols="30" rows="10" name="question" value={question} {...register("question", { required:'maximum 80 characters', minLength:15 ,maxLength:150 })}  onChange={(e)=> setQuestion(e.target.value)}></textarea>
+                                {errors.question && <span className="errorMessage">Question cannot be longer than 80 characters</span>}
+                        </div>
+                        <div className="form-field" onMouseEnter={handleSubmit(onFormSubmit)} >
+                            {!errors.name & !errors.email & !errors.subject & !errors.question ?  <div className="iconbtn" data-toggle="tooltip" data-placement="bottom" title="send email">
+                                <button className="submitBtn"  type="submit"><SendIcon/> </button>
+                                {/* <button>Submit</button> */}
+                        </div>: <div>Please check all fields</div>}
+                        </div>
+                    </form>
+                </div>
+  
                 <div className="right-content">
                 
                     {info.map(item => <ContactItem 
@@ -132,100 +126,6 @@ return (
 }
 
 const ContactPageStyled = styled.section`
-/* .container {
-	min-width: 700px;
-	min-height: 350px;
-	border-radius: 20px;
-	position: relative;
-	-webkit-transition: 1.5s ease-in-out;
-	transition: 1.5s ease-in-out;
-	transform-style: preserve-3d;
-}
-
-.side {
-	position: absolute;
-	text-align: center;
-	width: 100%;
-	height: 100%;
-	padding: 20px 50px;
-	color: #fff;
-	transform-style: preserve-3d;
-	backface-visibility: hidden;
-	border-radius: 20px;
-}
-.content {
-	transform: translatez(70px) scale(0.8);
-	line-height: 1.5em;
-}
-.content h1 {
-	position: relative;
-}
-.content p {
-	margin-top: 50px;
-	line-height: 2em;
-}
-.content h1:before {
-	content: "";
-	position: absolute;
-	bottom: -20px;
-	height: 3px;
-	background-color: #3e3;
-	width: 70px;
-	left: 50%;
-	transform: translateX(-50%);
-}
-.front {
-	z-index: 0;
-	background-size: 100vh;
-	background-size: cover;
-	background-image: url(https://livewallpaperhd.com/wp-content/uploads/2017/07/Dark-Elegant-Wallpaper.jpg);
-}
-.back {
-   
-	background-color: #333;
-	transform: rotateY(180deg);
-	z-index: 0;
-	padding-top: 10px;
-	background-image: url(https://userscontent2.emaze.com/images/f9538183-0ff9-478f-b964-c8ab90421e3b/3d28e192fda5c17250f96a2779c84475.jpg);
-}
-.container:hover {
-	-webkit-transform: rotateY(180deg);
-	transform: rotateY(180deg);
-    
-}
-form {
-	text-align: left;
-}
-.back h1 {
-	margin: 0;
-}
-form label,
-form input {
-	display: block;
-}
-form input,
-form textarea {
-	background: transparent;
-	border: 0;
-	border-bottom: 2px solid #444;
-	padding: 5px;
-	width: 100%;
-	color: #fff;
-}
-form label {
-	margin: 15px 0;
-}
-form input[type="submit"] {
-	display: block;
-	width: auto;
-	margin: 10px auto;
-	padding: 5px 10px;
-	border: 3px solid #555;
-	border-radius: 4px;
-	color: #fff;
-	cursor: pointer;
-} */
-/* my button style  */
 .white-mode {
 	text-decoration: none;
 	padding: 7px 10px;
@@ -238,7 +138,6 @@ form input[type="submit"] {
 	bottom: 15px;
 	font-family: "Montserrat";
 }
-
 .white-mode:hover {
 	background-color: #fff;
 	color: #122;
@@ -308,6 +207,14 @@ input{
      background-color:white;
      opacity:.4;
    } */
+}
+.err {
+    background-color:red;
+    
+    opacity:.3;
+}
+   .errorMessage ,.requiredStar {
+       color:red;
    }
 textarea{
    background-color:transparent;
@@ -318,15 +225,18 @@ textarea{
    width: 100%;
    padding:20px;
    }
-.iconbtn{
+   .submitBtn{
+  background: black;
+  padding: 0px;
+  border: none;
+  color: var(--icon-green-color);
   cursor:pointer;
-  font-size: 20px;
-  color: var(--icon-green-color)
-      }
     }
+   }
   } 
 }
 `
+
 export default ContactScreen
 
 
