b95b6b7d49ae0d560e8aea3286307ddcd79454ad
Yves Loeys <yvesloeys@MBP-Yves.attlocal.net>
2022-04-13 19:03:52 -0700

    added new controllers

--- backend/controllers/infoController.js
+++ backend/controllers/infoController.js
@@ -11,3 +11,42 @@ const info = asyncHandler(async (req, res) => {
 });
 
 module.exports = info;
+
+// router.route("/add").post((req, res) => {
+//   const {
+//     first_name,
+//     last_name,
+//     languages,
+//     location,
+//     services,
+//     bio,
+//     email1,
+//     email2,
+//     phone1,
+//     phone2,
+//     Address,
+//     PageLikes,
+//   } = req.body;
+
+//   const newInfo = new Info({
+//     first_name,
+//     last_name,
+//     languages,
+//     location,
+//     services,
+//     bio,
+//     email1,
+//     email2,
+//     phone1,
+//     phone2,
+//     Address,
+//     PageLikes,
+//   });
+
+//   newInfo
+//     .save()
+//     .then(() => res.json("info added"))
+//     .catch((err) => res.status(400).json("Error: " + err));
+// });
+
+//module.exports = router;
